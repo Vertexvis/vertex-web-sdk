9579c37f686c25c6d1bceeb57cc6c4015c17c6ab
Madison Ehlers <madison.ehlers@vertexvis.com>
2020-07-24 09:47:36 -0500

    defaulting values and only setting diffuse

--- packages/viewer/src/scenes/colorMaterial.ts
+++ packages/viewer/src/scenes/colorMaterial.ts
@@ -6,29 +6,44 @@ import { Color } from '@vertexvis/utils';
 export interface ColorMaterial {
   opacity: number;
   glossiness: number;
-  ambient: Color.Color;
   diffuse: Color.Color;
+  ambient: Color.Color;
   specular: Color.Color;
   emissive: Color.Color;
 }
 
+/**
+ * This helper converts a hex string to a ColorMaterial object
+ * @param hex
+ * @param opacity
+ */
 export const fromHex = (hex: string, opacity?: number): ColorMaterial => {
   const color: Color.Color = Color.fromHexString(hex);
 
   return {
-    opacity: 100,
+    opacity: opacity || 100,
     glossiness: opacity || 10,
-    ambient: {
-      ...color,
-    },
     diffuse: {
       ...color,
     },
+    ambient: {
+      r: 0.7,
+      g: 0.75,
+      b: 0,
+      a: 1,
+    },
+
     specular: {
-      ...color,
+      r: 0.2,
+      g: 0.2,
+      b: 0.2,
+      a: 64,
     },
     emissive: {
-      ...color,
+      r: 0.1,
+      g: 0,
+      b: 0.1,
+      a: 1,
     },
   };
 };
