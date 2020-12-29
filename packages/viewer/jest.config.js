41af59d57b5240e07e7bf93b6f4363d87f39eba6
Madison Ehlers <madison.ehlers@vertexvis.com>
2020-12-29 12:10:03 -0600

    removing jest changes

--- packages/viewer/jest.config.js
+++ packages/viewer/jest.config.js
@@ -1,6 +1,3 @@
 const jestConfig = require('@stencil/core/testing/jest-preset.js');
 
-module.exports = {
-  ...jestConfig,
-  setupFilesAfterEnv: ['<rootDir>/src/__setup__/setupEvents'],
-};
+module.exports = jestConfig;
