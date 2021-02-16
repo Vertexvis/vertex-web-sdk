92b759a437de07b749265fc35b3661f0ba3866bf
Dan Schultz <dan.schultz@vertexvis.com>
2021-02-16 16:49:59 -0600

    Fix test

--- packages/viewer/src/interactions/flyToPartKeyInteraction.ts
+++ packages/viewer/src/interactions/flyToPartKeyInteraction.ts
@@ -11,7 +11,7 @@ export class FlyToPartKeyInteraction
   ) {}
 
   public predicate(keyState: KeyState): boolean {
-    return keyState['Alt'];
+    return keyState['Alt'] === true;
   }
 
   public async fn(e: TapEventDetails): Promise<void> {
