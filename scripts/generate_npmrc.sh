98f8630f14b0d344768f7b8e4d2c4b433332fac6
Dan Schultz <dan.schultz@vertexvis.com>
2021-02-18 19:03:47 -0600

    Use Yarn workspaces

--- scripts/generate_npmrc.sh
+++ scripts/generate_npmrc.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-packages=`jq -r '.packages[]' ./lerna.json`
+packages=`jq -r '.workspaces[]' ./package.json`
 package_directories=($packages)
 
 for package_path in "${package_directories[@]}"; do
