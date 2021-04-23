1c2b1e398c284acb278b5104e312c06f4dd7da54
Jeff Murray <jeff.murray@vertexvis.com>
2021-04-23 13:49:28 -0500

    More tests

--- packages/geometry/src/__tests__/matrix4.test.ts
+++ packages/geometry/src/__tests__/matrix4.test.ts
@@ -23,3 +23,142 @@ describe(Matrix4.multiplyVector3, () => {
     ).toEqual(Vector4.create(7, 7, 7, 7));
   });
 });
+
+describe(Matrix4.multiplyVector4, () => {
+  it('returns correct value given a Matrix4 and Vector3', () => {
+    const values = new Array(16).fill(1);
+    expect(
+      Matrix4.multiplyVector4(
+        Matrix4.create(values),
+        Vector4.create(2, 2, 2, 2)
+      )
+    ).toEqual(Vector4.create(8, 8, 8, 8));
+  });
+});
+
+describe(Matrix4.inverse, () => {
+  it('returns the inverse of a provided matrix', () => {
+    const matrix = Matrix4.create([
+      1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      1,
+    ]);
+
+    expect(Matrix4.inverse(matrix)).toEqual([
+      1 / 4,
+      1 / 4,
+      1 / 4,
+      -1 / 4,
+      1 / 4,
+      1 / 4,
+      -1 / 4,
+      1 / 4,
+      1 / 4,
+      -1 / 4,
+      1 / 4,
+      1 / 4,
+      -1 / 4,
+      1 / 4,
+      1 / 4,
+      1 / 4,
+    ]);
+  });
+});
+
+describe(Matrix4.determinant, () => {
+  it('returns the correct determinant of a matrix', () => {
+    const matrix = Matrix4.create([
+      1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      -1,
+      1,
+      1,
+      1,
+    ]);
+
+    expect(Matrix4.determinant(matrix)).toEqual(-16);
+  });
+});
+
+describe(Matrix4.multiply, () => {
+  it('returns the result of multiplying two matrices', () => {
+    const matrix1 = Matrix4.create([
+      1,
+      1,
+      1,
+      1,
+      2,
+      2,
+      2,
+      2,
+      3,
+      3,
+      3,
+      3,
+      4,
+      4,
+      4,
+      4,
+    ]);
+    const matrix2 = Matrix4.create([
+      4,
+      4,
+      4,
+      4,
+      3,
+      3,
+      3,
+      3,
+      2,
+      2,
+      2,
+      2,
+      1,
+      1,
+      1,
+      1,
+    ]);
+
+    expect(Matrix4.multiply(matrix1, matrix2)).toEqual([
+      10,
+      10,
+      10,
+      10,
+      20,
+      20,
+      20,
+      20,
+      30,
+      30,
+      30,
+      30,
+      40,
+      40,
+      40,
+      40,
+    ]);
+  });
+});
