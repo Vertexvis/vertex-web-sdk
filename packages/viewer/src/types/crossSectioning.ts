60f73d1ecf0c99c1440d5596364bdf15068682c6
Jared Dellitt <jareddellitt@users.noreply.github.com>
2021-05-13 11:01:37 -0500

    Add cross sectioning highlightColor (#182)

--- packages/viewer/src/types/crossSectioning.ts
+++ packages/viewer/src/types/crossSectioning.ts
@@ -1,4 +1,5 @@
 import { Vector3 } from '@vertexvis/geometry';
+import { Color } from '@vertexvis/utils';
 
 export interface SectionPlane {
   normal: Vector3.Vector3;
@@ -7,10 +8,12 @@ export interface SectionPlane {
 
 export interface CrossSectioning {
   sectionPlanes: Array<SectionPlane>;
+  highlightColor?: Color.Color;
 }
 
 export function create(data: Partial<CrossSectioning> = {}): CrossSectioning {
   return {
     sectionPlanes: data.sectionPlanes || [],
+    highlightColor: data.highlightColor,
   };
 }
