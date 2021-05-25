7926b17c1b52a47a2f5ebb2e5171f9ec43e2d24d
Dan Schultz <dan.schultz@vertexvis.com>
2021-05-25 17:44:16 -0500

    Add skeleton for core library

---
+++ packages/viewer-core/jest.config.js
@@ -0,0 +1,18 @@
+const jestConfig = require('@vertexvis/jest-config-vertexvis/jest.config');
+
+module.exports = {
+  ...jestConfig,
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.json',
+    },
+  },
+};
