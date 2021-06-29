edd5290ec0b762238da3f4fd5801951aa9f3d85f
Dan Schultz <dan.schultz@vertexvis.com>
2021-06-28 23:17:02 -0600

    Occlude webgl geometry with server depth shader

--- packages/viewer/src/components/viewer-threejs-renderer/shaders.ts
+++ packages/viewer/src/components/viewer-threejs-renderer/shaders.ts
@@ -8,6 +8,31 @@ export const vertexShader = `
   }
 `;
 
+export const identityVertexShader = `
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = vec4(position, 1.0);
+  }
+`;
+
+const utils = `
+  struct Rect {
+    float x;
+    float y;
+    float width;
+    float height;
+  };
+
+  bool containsPoint(vec2 pt, Rect rect) {
+    return pt.x >= rect.x && pt.x <= rect.x + rect.width && pt.y >= rect.y && pt.y <= rect.y + rect.height;
+  }
+
+  float lerp(float a, float b, float t) {
+    return a + (b - a) * t;
+  }
+`;
+
 export const fragmentShader = `
   #include <packing>
 
@@ -52,6 +77,59 @@ export const fragmentShader = `
     float alpha = depth < serverDepth ? color.a : 0.0;
 
     gl_FragColor = color;
+    gl_FragColor.a = alpha;
+  }
+`;
+
+export const serverDepth16FragmentShader = `
+  #include <packing>
+
+  ${utils}
+
+  varying vec2 vUv;
+  uniform sampler2D diffuseTexture;
+  uniform sampler2D depthTexture;
+  uniform sampler2D serverDepthTexture;
+  uniform mat3 serverDepthMatrix;
+  uniform Rect serverDepthRect;
+  uniform vec2 dimensions;
+  uniform float cameraNear;
+  uniform float cameraFar;
+  uniform float serverNear;
+  uniform float serverFar;
+
+  float getWorldDepth(sampler2D depthSampler, vec2 coord) {
+    float fragCoordZ = texture2D(depthSampler, coord).x;
+    float viewZ = perspectiveDepthToViewZ(fragCoordZ, cameraNear, cameraFar);
+    return viewZToOrthographicDepth(viewZ, cameraNear, cameraFar);
+  }
+
+  float getNormalizedServerDepth(sampler2D serverDepth, vec2 coord) {
+    vec3 scaledPos = vec3(coord.x - serverDepthRect.x, coord.y - serverDepthRect.y, 1.0) * serverDepthMatrix;
+    ivec2 pos = ivec2(scaledPos.x, scaledPos.y);
+
+    if (containsPoint(coord, serverDepthRect)) {
+      vec4 values = texelFetch(serverDepth, pos, 0);
+      // Need to investigate if we need to use the same projected math from DepthBuffer here.
+      return values.r;
+    } else {
+      return 1.0;
+    }
+  }
+
+  float getServerDepthInWorld(sampler2D serverDepth, vec2 coord) {
+    float depth = getNormalizedServerDepth(serverDepth, coord);
+    float localDepth = lerp(serverNear, serverFar, depth);
+    return localDepth / (cameraFar - cameraNear);
+  }
+
+  void main() {
+    float worldDepth = getWorldDepth(depthTexture, vUv);
+    float serverDepth = getServerDepthInWorld(serverDepthTexture, gl_FragCoord.xy);
+    vec4 color = texture2D(diffuseTexture, vUv);
+    float alpha = worldDepth < serverDepth ? color.a : 0.0;
+
+    gl_FragColor.rgb = color.rgb;
     gl_FragColor.a = alpha;
   }
 `;
@@ -105,8 +183,8 @@ export function computeServerDepthTextureMatrix(
     depthBuffer.imageDimensions
   );
   const scale = viewport.calculateFrameScale(depthBuffer);
-  const scaleX = viewport.width / drawRect.width;
-  const scaleY = viewport.height / drawRect.height;
+  const scaleX = 1 / (depthBuffer.scale * scale.x);
+  const scaleY = 1 / (depthBuffer.scale * scale.y);
 
   // const tx = -drawRect.x / viewport.width - 0.5;
   // const ty = -drawRect.y / viewport.height - 0.5;
@@ -122,9 +200,57 @@ export function computeServerDepthTextureMatrix(
   ];
   /* eslint-enable prettier/prettier */
 
-  console.log('m', res, drawRect, depthBuffer.rect);
+  // console.log('draw rect', drawRect, depthBuffer.imageDimensions);
+
   return res;
 }
+
+export const drawServerDepth16Shader = `
+  #include <packing>
+
+  struct Rect {
+    float x;
+    float y;
+    float width;
+    float height;
+  };
+
+  varying vec2 vUv;
+  uniform sampler2D diffuseTexture;
+  uniform sampler2D depthTexture;
+  uniform sampler2D serverDepthTexture;
+  uniform mat3 serverDepthMatrix;
+  uniform Rect serverDepthRect;
+  uniform vec2 dimensions;
+  uniform float cameraNear;
+  uniform float cameraFar;
+  uniform float serverNear;
+  uniform float serverFar;
+
+  bool containsPoint(vec2 pt, Rect rect) {
+    return pt.x >= rect.x && pt.x <= rect.x + rect.width && pt.y >= rect.y && pt.y <= rect.y + rect.height;
+  }
+
+  float readServerDepth(sampler2D serverDepth, vec2 coord) {
+    vec3 scaledPos = vec3(coord.x - serverDepthRect.x, coord.y - serverDepthRect.y, 1.0) * serverDepthMatrix;
+    ivec2 pos = ivec2(scaledPos.x, scaledPos.y);
+
+    if (containsPoint(coord, serverDepthRect)) {
+      // return 1.0;
+      vec4 values = texelFetch(serverDepth, pos, 0);
+      return values.r;
+    } else {
+      return 1.0;
+    }
+  }
+
+  void main() {
+    float serverDepth = readServerDepth(serverDepthTexture, gl_FragCoord.xy);
+
+    gl_FragColor.rgb = texture(diffuseTexture, vUv).rgb + vec3(serverDepth);
+    gl_FragColor.a = 1.0;
+  }
+`;
 
 export const debugServerDepth16Shader = `
   #include <packing>
@@ -178,6 +304,40 @@ export const debugServerDepth16Shader = `
     vec3 coord = (vec3(vUv.x, vUv.y, 1.0) - 0.5) * serverDepthMatrix + (0.5 * scale);
     // vec3 coords = vec3(vUv.x, vUv.y, 1.0) * serverDepthMatrix;
     gl_FragColor.rgb = vec3(texture2D(serverDepthTexture, coord.xy).r);
+    gl_FragColor.a = 1.0;
+  }
+`;
+
+export const debugFragPosition = `
+  #include <packing>
+
+  struct Rect {
+    float x;
+    float y;
+    float width;
+    float height;
+  };
+
+  varying vec2 vUv;
+  uniform sampler2D diffuseTexture;
+  uniform sampler2D depthTexture;
+  uniform sampler2D serverDepthTexture;
+  uniform mat3 serverDepthMatrix;
+  uniform Rect serverDepthRect;
+  uniform vec2 dimensions;
+  uniform float cameraNear;
+  uniform float cameraFar;
+  uniform float serverNear;
+  uniform float serverFar;
+
+  float readServerDepth(sampler2D serverDepth, vec2 coord) {
+    return texture(serverDepth, coord / viewPortSize).r;
+  }
+
+  void main() {
+    float serverDepth = readServerDepth(serverDepthTexture, gl_FragCoord.xy);
+
+    gl_FragColor.rgb = vec3(serverDepth);
     gl_FragColor.a = 1.0;
   }
 `;
