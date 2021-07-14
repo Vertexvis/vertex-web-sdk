47982e81713593be661f1ad92cbb07731e225e99
Dan Schultz <dan.schultz@vertexvis.com>
2021-07-13 18:49:11 -0600

    Fix formatting script

--- packages/viewer/scripts/format-examples.js
+++ packages/viewer/scripts/format-examples.js
@@ -1,22 +1,23 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const { execSync } = require('child_process');
 
-process.stdin.once('readable', async function () {
-  const typeDefTxt = String(process.stdin.read());
-  const docExampleMatches = typeDefTxt.matchAll(/``` (.*) ```/g);
-  const typeDefNewFormatted = [...docExampleMatches].reduce(function (
-    typeDefNewFormatted,
-    exampleMatch
-  ) {
-    const formattedMatch = `\`\`\`\n${formatExample(
-      exampleMatch[1]
-    )}\`\`\``.replace(/^/gm, '* ');
+const stdin = fs.readFileSync(process.stdin.fd, 'utf-8');
 
-    return typeDefNewFormatted.replace(exampleMatch[0], `\n${formattedMatch}`);
-  },
-  typeDefTxt);
-  process.stdout.write(typeDefNewFormatted);
-});
+const docExampleMatches = stdin.matchAll(/``` (.*) ```/g);
+const typeDefNewFormatted = [...docExampleMatches].reduce(function (
+  typeDefNewFormatted,
+  exampleMatch
+) {
+  const formattedMatch = `\`\`\`\n${formatExample(
+    exampleMatch[1]
+  )}\`\`\``.replace(/^/gm, '* ');
+
+  return typeDefNewFormatted.replace(exampleMatch[0], `\n${formattedMatch}`);
+},
+stdin);
+
+process.stdout.write(typeDefNewFormatted);
 
 function formatExample(example) {
   return String(
