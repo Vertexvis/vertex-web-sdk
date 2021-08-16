673fdd2c31744dc9d90d719b9e7b30fa7023a8c3
Madison Ehlers <madison.ehlers@vertexvis.com>
2021-08-16 12:10:26 -0500

    Default the pool size if window is undefined

--- packages/viewer/src/workers/png-decoder-pool.ts
+++ packages/viewer/src/workers/png-decoder-pool.ts
@@ -7,6 +7,8 @@ type DecodePngPool = Pool<DecodePngFn>;
 let workerLoader: Promise<DecodePngModule> | undefined;
 let poolLoader: Promise<DecodePngPool> | undefined;
 
+const DEFAULT_POOL_SIZE = 1;
+
 function loadWorkerModule(): Promise<DecodePngModule> {
   if (workerLoader == null) {
     workerLoader = loadWorker();
@@ -14,17 +16,22 @@ function loadWorkerModule(): Promise<DecodePngModule> {
   return workerLoader;
 }
 
+function getPoolSize(): number {
+  if (typeof window !== 'undefined') {
+    return Math.ceil(window.navigator.hardwareConcurrency / 4);
+  } else {
+    return DEFAULT_POOL_SIZE;
+  }
+}
+
 async function getPool(): Promise<DecodePngPool> {
   if (poolLoader == null) {
     poolLoader = loadWorkerModule().then(({ spawnPool }) =>
-      spawnPool({
-        size: Math.ceil(window.navigator.hardwareConcurrency / 4),
-      })
+      spawnPool({ size: getPoolSize() })
     );
   }
   return poolLoader;
 }
-
 export const decodePng: DecodePngFn = async (bytes) => {
   const pool = await getPool();
   return pool.queue((decode) => {
@@ -33,6 +40,4 @@ export const decodePng: DecodePngFn = async (bytes) => {
 };
 
 // Prefetch the worker and initialize the pool.
-if (typeof window !== 'undefined') {
-  getPool();
-}
+getPool();
