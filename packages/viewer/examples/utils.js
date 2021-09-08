7ca3335cff0cc431c8630ca55b28d5b135e9d469
Gerrick Bivins <gerrick.bivins@vertexvis.com>
2021-09-08 11:52:55 -0500

    relocate utils from helper.js to common utils

--- packages/viewer/examples/utils.js
+++ packages/viewer/examples/utils.js
@@ -11,3 +11,48 @@ export async function addGeometryLoaderMeshes(renderer, parent, meshes) {
     renderer.draw();
   }
 }
+
+export async function loadViewerWithQueryParams(viewer) {
+  const clientId = readDefaultClientId();
+  const key = readDefaultStreamKey();
+  viewer.setAttribute('client-id', clientId);
+  console.log(readUrlParams());
+  await viewer.load(`urn:vertexvis:stream-key:${key}`);
+  console.log('loaded');
+}
+
+export function readDefaultClientId() {
+  const urlParams = readUrlParams();
+
+  return urlParams.clientid || '';
+}
+
+export function readDefaultStreamKey() {
+  const urlParams = readUrlParams();
+
+  return urlParams.streamkey || '';
+}
+
+export function readDebugFeatureMap() {
+  const urlParams = readUrlParams();
+  return urlParams.debugfeaturemap || '';
+}
+
+export function readSceneId() {
+  const urlParams = readUrlParams();
+  return urlParams.sceneid || '464b4f18-387e-4a8e-8d6f-fffe289dd892'; //TODO: Lookout!!!
+}
+
+function readUrlParams() {
+  return window.location.search
+    .slice(1, window.location.search.length)
+    .split('&')
+    .reduce((result, value) => {
+      const param = value.split(/=(.+)/, 2);
+
+      return {
+        ...result,
+        [param[0].replace('-', '').toLowerCase()]: param[1],
+      };
+    }, {});
+}
