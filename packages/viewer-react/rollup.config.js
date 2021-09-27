ae4f8ab71c40dd99e1bfc5eb4ea96d8ac3c461df
Dan Schultz <dan.schultz@vertexvis.com>
2021-09-27 10:17:10 -0600

    Remove unused build code from viewer-react (#253)

--- packages/viewer-react/rollup.config.js
+++ packages/viewer-react/rollup.config.js
@@ -1,17 +1,3 @@
 import { rollupConfig } from '@vertexwebsdk/build';
 
 export default rollupConfig({ external: ['@vertexvis/viewer/loader'] });
-
-// import commonjs from '@rollup/plugin-commonjs';
-// import resolve from '@rollup/plugin-node-resolve';
-// import typescript from 'rollup-plugin-typescript2';
-
-// export default {
-//   input: './src/index.ts',
-//   output: {
-//     file: './dist/bundle.esm.js',
-//     format: 'esm',
-//   },
-//   external: ['@vertexvis/viewer/loader'],
-//   plugins: [commonjs(), resolve(), typescript()],
-// };
