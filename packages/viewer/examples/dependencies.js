e5954cfcbd1ecf0ab22acbca799b5849feed2106
Dan Schultz <dan.schultz@vertexvis.com>
2021-09-30 10:00:19 -0600

    Fix threejs dependencies

---
+++ packages/viewer/examples/dependencies.js
@@ -0,0 +1,4 @@
+import * as THREE from 'https://cdn.skypack.dev/three@0.131.3';
+import { TransformControls } from 'https://cdn.skypack.dev/three@0.131.3/examples/jsm/controls/TransformControls.js';
+
+export { THREE, TransformControls };
