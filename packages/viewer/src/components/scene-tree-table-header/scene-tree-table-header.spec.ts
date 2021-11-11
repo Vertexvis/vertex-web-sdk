9c999231053c4d8ec32001a5599fc5d2f25bd50c
Jeff Murray <jeff.murray@vertexvis.com>
2021-11-11 16:15:30 -0600

    Fix test

--- packages/viewer/src/components/scene-tree-table-header/scene-tree-table-header.spec.ts
+++ packages/viewer/src/components/scene-tree-table-header/scene-tree-table-header.spec.ts
@@ -2,17 +2,16 @@ import { newSpecPage, SpecPage } from '@stencil/core/testing';
 import { SceneTreeTableHeader } from './scene-tree-table-header';
 
 describe('<vertex-scene-tree-table-header>', () => {
-  it('renders the specified label', async () => {
+  it('renders', async () => {
     const { header } = await newComponentSpec({
       html: `
-        <vertex-scene-tree-table-header label="label-value">
+        <vertex-scene-tree-table-header>
+          label-value
         </vertex-scene-tree-table-header>
       `,
     });
 
-    expect(header.shadowRoot?.querySelector('div.header')?.textContent).toMatch(
-      'label-value'
-    );
+    expect(header.shadowRoot?.querySelector('div.header')).not.toBeNull();
   });
 });
 
