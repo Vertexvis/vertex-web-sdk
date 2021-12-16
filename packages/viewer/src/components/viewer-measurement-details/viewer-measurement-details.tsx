0f539bb19bc51afa37f53419ac57a55584853b51
chrisgray-vertex <chris.gray@vertexvis.com>
2021-12-16 13:58:56 -0600

    cleanup

--- packages/viewer/src/components/viewer-measurement-details/viewer-measurement-details.tsx
+++ packages/viewer/src/components/viewer-measurement-details/viewer-measurement-details.tsx
@@ -272,7 +272,6 @@ export class ViewerMeasurementDetails {
     const hidden = this.hiddenDetails ?? [];
 
     this.summary = baseSummary;
-    console.log('baseSummary', baseSummary);
     this.visibleSummary = (
       Object.keys(baseSummary) as Array<keyof MeasurementDetailsSummary>
     )
