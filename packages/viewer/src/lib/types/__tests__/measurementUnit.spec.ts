d2f3e66a4129d077bb0c29da10a2181b144bc8da
chrisgray-vertex <chris.gray@vertexvis.com>
2021-12-16 13:57:33 -0600

    units tests

--- packages/viewer/src/lib/types/__tests__/measurementUnit.spec.ts
+++ packages/viewer/src/lib/types/__tests__/measurementUnit.spec.ts
@@ -1,16 +1,26 @@
 import { Vector3 } from '@vertexvis/geometry';
-import { DistanceUnits, DistanceUnitType } from '../measurementUnits';
+import {
+  AreaUnits,
+  DistanceUnits,
+  DistanceUnitType,
+} from '../measurementUnits';
 
-interface Test {
+interface DisanceUnitTest {
+  unitType: DistanceUnitType;
+  worldUnit: number;
+  realUnit: number;
+}
+
+interface AreaUnitTest {
   unitType: DistanceUnitType;
   worldUnit: number;
   realUnit: number;
 }
 
 describe(DistanceUnits, () => {
-  const tests: Test[] = [
+  const tests: DisanceUnitTest[] = [
     { unitType: 'millimeters', worldUnit: 1, realUnit: 1 },
-    { unitType: 'centimeters', worldUnit: 1, realUnit: 1 / 100 },
+    { unitType: 'centimeters', worldUnit: 1, realUnit: 1 / 10 },
     { unitType: 'meters', worldUnit: 1, realUnit: 1 / 1000 },
     { unitType: 'inches', worldUnit: 1, realUnit: 1 / 25.4 },
     { unitType: 'feet', worldUnit: 1, realUnit: 1 / (25.4 * 12) },
@@ -60,6 +70,43 @@ describe(DistanceUnits, () => {
         const realPt = Vector3.create(realUnit, realUnit, realUnit);
         expect(measurementUnit.convertRealPointToWorld(realPt)).toEqual(
           worldPt
+        );
+      }
+    });
+  });
+});
+
+describe(AreaUnits, () => {
+  const tests: AreaUnitTest[] = [
+    { unitType: 'millimeters', worldUnit: 1, realUnit: 1 },
+    { unitType: 'centimeters', worldUnit: 1, realUnit: 1 / Math.pow(10, 2) },
+    { unitType: 'meters', worldUnit: 1, realUnit: 1 / Math.pow(1000, 2) },
+    { unitType: 'inches', worldUnit: 1, realUnit: 1 / Math.pow(25.4, 2) },
+    { unitType: 'feet', worldUnit: 1, realUnit: 1 / Math.pow(25.4 * 12, 2) },
+    {
+      unitType: 'yards',
+      worldUnit: 1,
+      realUnit: 1 / Math.pow(25.4 * 12 * 3, 2),
+    },
+  ];
+
+  describe(AreaUnits.prototype.convertWorldValueToReal, () => {
+    it('translates a world value to real value', () => {
+      for (const { unitType, realUnit, worldUnit } of tests) {
+        const measurementUnit = new AreaUnits(unitType);
+        expect(measurementUnit.convertWorldValueToReal(worldUnit)).toEqual(
+          realUnit
+        );
+      }
+    });
+  });
+
+  describe(AreaUnits.prototype.convertRealValueToWorld, () => {
+    it('translates a real value to world value', () => {
+      for (const { unitType, realUnit, worldUnit } of tests) {
+        const measurementUnit = new AreaUnits(unitType);
+        expect(measurementUnit.convertRealValueToWorld(realUnit)).toEqual(
+          worldUnit
         );
       }
     });
