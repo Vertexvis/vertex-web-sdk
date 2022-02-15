2b01b9e51dd2dbfd7ba3819e61bcf9e955a39d3d
Jeff Murray <jeff.murray@vertexvis.com>
2022-02-15 14:56:38 -0600

    Use ResizeObserver polyfill

--- packages/viewer/src/components/viewer/viewer.tsx
+++ packages/viewer/src/components/viewer/viewer.tsx
@@ -15,6 +15,7 @@ import { Dimensions, Point } from '@vertexvis/geometry';
 import { toProtoDuration, WebSocketClientImpl } from '@vertexvis/stream-api';
 import { Color, Disposable, EventDispatcher, UUID } from '@vertexvis/utils';
 import classnames from 'classnames';
+import { ResizeObserver } from 'resize-observer';
 
 import {
   FeatureHighlightOptions,
