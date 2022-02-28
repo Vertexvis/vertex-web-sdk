4c1229c46e794f9a6ed9ad23823892e904e46e07
Jeff Murray <jeff.murray@vertexvis.com>
2022-02-28 16:47:07 -0600

    Additional test

--- packages/viewer/src/lib/types/__tests__/frame.spec.ts
+++ packages/viewer/src/lib/types/__tests__/frame.spec.ts
@@ -1,9 +1,9 @@
-import { Line3, Vector3 } from '@vertexvis/geometry';
+import { Line3, Matrix4, Vector3 } from '@vertexvis/geometry';
 
-import { FrameCameraBase } from '../frame';
+import { FrameOrthographicCamera, FramePerspectiveCamera } from '../frame';
 
-describe(FrameCameraBase, () => {
-  const camera = new FrameCameraBase(
+describe(FramePerspectiveCamera, () => {
+  const camera = new FramePerspectiveCamera(
     Vector3.scale(2, Vector3.back()),
     Vector3.origin(),
     Vector3.up(),
@@ -13,7 +13,7 @@ describe(FrameCameraBase, () => {
     45
   );
 
-  describe(FrameCameraBase.prototype.isPointBehindNear, () => {
+  describe(FramePerspectiveCamera.prototype.isPointBehindNear, () => {
     it('returns true if world point behind near plane', () => {
       const pt = camera.position;
       expect(camera.isPointBehindNear(pt)).toBe(true);
@@ -25,7 +25,7 @@ describe(FrameCameraBase, () => {
     });
   });
 
-  describe(FrameCameraBase.prototype.intersectLineWithNear, () => {
+  describe(FramePerspectiveCamera.prototype.intersectLineWithNear, () => {
     it('returns point on near plane if line intersects', () => {
       const line = Line3.create({
         start: Vector3.origin(),
@@ -44,6 +44,65 @@ describe(FrameCameraBase, () => {
 
       const pt = camera.intersectLineWithNear(line);
       expect(pt).toBeUndefined();
+    });
+  });
+});
+
+describe(FrameOrthographicCamera, () => {
+  const camera = new FrameOrthographicCamera(
+    Vector3.scale(2, Vector3.back()),
+    Vector3.origin(),
+    Vector3.up(),
+    1,
+    10,
+    1,
+    45
+  );
+
+  describe(FrameOrthographicCamera.prototype.isPointBehindNear, () => {
+    it('returns true if world point behind near plane', () => {
+      const pt = camera.position;
+      expect(camera.isPointBehindNear(pt)).toBe(true);
+    });
+
+    it('returns false if world point in front of near plane', () => {
+      const pt = Vector3.origin();
+      expect(camera.isPointBehindNear(pt)).toBe(false);
+    });
+  });
+
+  describe(FrameOrthographicCamera.prototype.intersectLineWithNear, () => {
+    it('returns point on near plane if line intersects', () => {
+      const line = Line3.create({
+        start: Vector3.origin(),
+        end: camera.position,
+      });
+
+      const pt = camera.intersectLineWithNear(line);
+      expect(pt).toEqual(Vector3.create(0, 0, camera.near));
+    });
+
+    it('returns undefined if line does not intersect near plane', () => {
+      const line = Line3.create({
+        start: Vector3.left(),
+        end: Vector3.right(),
+      });
+
+      const pt = camera.intersectLineWithNear(line);
+      expect(pt).toBeUndefined();
+    });
+
+    it('computes the orthographic projection matrix', () => {
+      expect(camera.projectionMatrix).toMatchObject(
+        Matrix4.makeOrthographic(
+          camera.left,
+          camera.right,
+          camera.bottom,
+          camera.top,
+          camera.near,
+          camera.far
+        )
+      );
     });
   });
 });
