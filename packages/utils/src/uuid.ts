31ea66875416e42f362827fde28a52b1ba0a7a24
Dan Schultz <dan.schultz@vertexvis.com>
2022-08-03 14:11:22 -0500

    [PLAT-1808] Add utility to parse MSB, LSB from UUID strings (#431)
    Add utility to parse MSB, LSB from UUID strings

--- packages/utils/src/uuid.ts
+++ packages/utils/src/uuid.ts
@@ -1,5 +1,28 @@
-import { v1 as uuid } from 'uuid';
+import Long from 'long';
+import { parse, v4 as uuid } from 'uuid';
 
 export type UUID = string;
 
-export const create = (): UUID => uuid();
+export interface UUIDMsbLsb {
+  msb: string;
+  lsb: string;
+}
+
+export function create(): UUID {
+  return uuid();
+}
+
+export function toMsbLsb(id: UUID): UUIDMsbLsb {
+  const bytes = parse(id);
+
+  let msb = Long.fromInt(0);
+  let lsb = Long.fromInt(0);
+  for (let i = 0; i < 8; i++) {
+    msb = msb.shiftLeft(8).or(bytes[i] & 0xff);
+  }
+  for (let i = 8; i < 16; i++) {
+    lsb = lsb.shiftLeft(8).or(bytes[i] & 0xff);
+  }
+
+  return { msb: msb.toString(), lsb: lsb.toString() };
+}
