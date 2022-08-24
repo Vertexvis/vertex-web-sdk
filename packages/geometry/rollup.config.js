012660e3544f66f5a3eb0d4111b8f3d187a1903a
Dan Schultz <dan.schultz@vertexvis.com>
2022-08-24 07:56:35 -0600

    [PLAT-1959] Fix SonarQube warnings (#436)
    Fix SonarQube warnings

--- packages/geometry/rollup.config.js
+++ packages/geometry/rollup.config.js
@@ -3,6 +3,6 @@ import { rollupCdnConfig, rollupConfig } from '@vertexwebsdk/build';
 const standardConfig = rollupConfig();
 const cdnConfig = rollupCdnConfig();
 
-export default typeof standardConfig === 'array'
+export default Array.isArray(standardConfig)
   ? [...standardConfig, cdnConfig]
   : [standardConfig, cdnConfig];
