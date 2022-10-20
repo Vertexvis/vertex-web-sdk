996d31046355a0e00c6cfb7592e5e7a41c41d895
Jeff Murray <jeff.murray@vertexvis.com>
2022-10-20 12:12:00 -0500

    Remove testing publish

--- scripts/publish_canary.sh
+++ scripts/publish_canary.sh
@@ -6,27 +6,25 @@ set -e
 
 . "$(pwd)"/scripts/utils.sh
 
-preid="testing"
-dist_tag="testing"
+preid="canary"
+dist_tag="canary"
 next_bump=`jq -r '.nextVersionBump' package.json`
 version=`jq -r '.version' lerna.json`
 next_version=`npx semver "$version" --increment "$next_bump"`
-# published_canary_versions=`npm view @vertexvis/viewer --json versions | jq --arg version "$next_version-" -r '.[] | select(contains($version))'`
+published_canary_versions=`npm view @vertexvis/viewer --json versions | jq --arg version "$next_version-" -r '.[] | select(contains($version))'`
 
-# echo $published_canary_versions
+if test -n "$published_canary_versions"
+then
+  published_version=`npx semver $(echo "$published_canary_versions") | tail -1`
+  echo "Detected published canary version $published_version"
 
-# if test -n "$published_canary_versions"
-# then
-#   published_version=`npx semver $(echo "$published_canary_versions") | tail -1`
-#   echo "Detected published canary version $published_version"
+  next_canary_version=`npx semver "$published_version" --increment prerelease`
+  echo "Publishing canary version $next_canary_version"
 
-#   next_canary_version=`npx semver "$published_version" --increment prerelease`
-#   echo "Publishing canary version $next_canary_version"
-
-#   # npx lerna version --no-push --no-git-tag-version --exact "$next_canary_version" --yes
-#   # git commit -am "Canary release $next_canary_version"
-#   # npx lerna publish from-package --canary --preid "$preid" --exact --dist-tag "$dist_tag" --yes
-# else
+  npx lerna version --no-push --no-git-tag-version --exact "$next_canary_version" --yes
+  git commit -am "Canary release $next_canary_version"
+  npx lerna publish from-package --canary --preid "$preid" --exact --dist-tag "$dist_tag" --yes
+else
   echo "No published canary version found for $next_version"
 
   next_canary_version=`npx semver "$version" --increment pre"$next_bump" --preid "$preid"`
@@ -35,5 +33,5 @@ next_version=`npx semver "$version" --increment "$next_bump"`
   npx lerna version --no-push --no-git-tag-version --exact "$next_canary_version" --yes
   git commit -am "Canary release $next_canary_version"
   npx lerna publish from-package --canary --preid "$preid" --exact --dist-tag "$dist_tag" --yes
-# fi
+fi
 
