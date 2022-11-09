b188cfb3e0d7a8f9590d28fae88b355dbedde697
Jeff Murray <jeff.murray@vertexvis.com>
2022-11-09 16:17:26 -0600

    [PLAT-2161] Scene tree error improvements (#451)
    * Add additional error details
    
    * Change token back
    
    * Revert always logging
    
    * Update tests
    
    * Add tests

--- packages/viewer/src/components/scene-tree/lib/errors.ts
+++ packages/viewer/src/components/scene-tree/lib/errors.ts
@@ -1,15 +1,19 @@
+import { CustomError } from '../../../lib/errors';
+
 export enum SceneTreeErrorCode {
   UNKNOWN = 0,
   SCENE_TREE_DISABLED = 1,
   MISSING_VIEWER = 2,
   DISCONNECTED = 3,
   SUBSCRIPTION_FAILURE = 4,
+  UNAUTHORIZED = 5,
 }
 
 export class SceneTreeErrorDetails {
   public readonly message: string;
 
   public constructor(
+    public readonly name: keyof typeof SceneTreeErrorCode,
     public readonly code: SceneTreeErrorCode,
     public readonly link?: string
   ) {
@@ -29,5 +33,43 @@ function getSceneTreeErrorMessage(code: SceneTreeErrorCode): string {
       return 'Disconnected from server.';
     case SceneTreeErrorCode.SUBSCRIPTION_FAILURE:
       return 'The tree was not able to receive subscription events';
+    case SceneTreeErrorCode.UNAUTHORIZED:
+      return 'The tree was unauthorized to connect to the server. The associated Viewer may not be connected.';
+  }
+}
+
+export class SceneTreeError extends CustomError {
+  public constructor(message: string, e?: Error) {
+    super(message, e);
+
+    // Allows for `instanceof` checks.
+    Object.setPrototypeOf(this, SceneTreeError.prototype);
+  }
+}
+
+export class SceneTreeUnauthorizedError extends SceneTreeError {
+  public constructor(message: string, e?: Error) {
+    super(message, e);
+
+    // Allows for `instanceof` checks.
+    Object.setPrototypeOf(this, SceneTreeUnauthorizedError.prototype);
+  }
+}
+
+export class SceneTreeConnectionCancelledError extends SceneTreeError {
+  public constructor(message: string, e?: Error) {
+    super(message, e);
+
+    // Allows for `instanceof` checks.
+    Object.setPrototypeOf(this, SceneTreeConnectionCancelledError.prototype);
+  }
+}
+
+export class SceneTreeOperationFailedError extends SceneTreeError {
+  public constructor(message: string, e?: Error) {
+    super(message, e);
+
+    // Allows for `instanceof` checks.
+    Object.setPrototypeOf(this, SceneTreeOperationFailedError.prototype);
   }
 }
