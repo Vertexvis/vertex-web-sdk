df163e9441caf730455718107cbe0c0826c4a5a3
Jeff Murray <jeff.murray@vertexvis.com>
2023-08-29 17:11:35 -0500

    Update with factory stream key

--- examples/walk-mode/main.js
+++ examples/walk-mode/main.js
@@ -1,14 +1,9 @@
-import { loadViewerWithQueryParams } from '../helpers.js';
-
 window.addEventListener('DOMContentLoaded', () => {
   main();
 });
 
 async function main() {
   await window.customElements.whenDefined('vertex-viewer');
-  const viewer = document.querySelector('vertex-viewer');
-
-  await loadViewerWithQueryParams(viewer);
 
   const walkModeTool = document.querySelector('vertex-viewer-walk-mode-tool');
   const increment = document.querySelector('#increment-speed');
@@ -50,10 +45,24 @@ async function main() {
   });
 
   teleportButton.addEventListener('click', () => {
-    walkModeTool.teleportMode = 'teleport';
+    if (walkModeTool.teleportMode === 'teleport') {
+      teleportButton.style = undefined;
+      walkModeTool.teleportMode = undefined;
+    } else {
+      teleportAndAlignButton.style = undefined;
+      teleportButton.style = "color: #0099cc";
+      walkModeTool.teleportMode = 'teleport';
+    }
   });
 
   teleportAndAlignButton.addEventListener('click', () => {
-    walkModeTool.teleportMode = 'teleport-and-align';
+    if (walkModeTool.teleportMode === 'teleport-and-align') {
+      teleportAndAlignButton.style = undefined;
+      walkModeTool.teleportMode = undefined;
+    } else {
+      teleportButton.style = undefined;
+      teleportAndAlignButton.style = "color: #0099cc";
+      walkModeTool.teleportMode = 'teleport-and-align';
+    }
   });
 }
