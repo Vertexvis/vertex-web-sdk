efe2ed98cffb968dc4be4815c86c30d0f7e22776
Madison Ehlers <madison.ehlers@vertexvis.com>
2024-02-27 15:21:56 -0600

    [PLAT-3931] Verifying the dom element & dom group have a transform applied prior to acting on it

--- packages/viewer/src/components/viewer-dom-group/utils.ts
+++ packages/viewer/src/components/viewer-dom-group/utils.ts
@@ -2,7 +2,8 @@ export function isVertexViewerDomGroup(
   el: unknown
 ): el is HTMLVertexViewerDomGroupElement {
   return (
-    (el instanceof HTMLElement && el.nodeName === 'VERTEX-VIEWER-DOM-GROUP') ||
-    (el instanceof HTMLElement && el.dataset.isDomGroupElement != null)
+    ((el instanceof HTMLElement && el.nodeName === 'VERTEX-VIEWER-DOM-GROUP') ||
+      (el instanceof HTMLElement && el.dataset.isDomGroupElement != null)) &&
+    (el as HTMLVertexViewerDomGroupElement).matrix != null
   );
 }
