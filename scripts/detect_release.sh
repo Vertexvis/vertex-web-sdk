972b3b38dfe873ed51563ac49b9a2ce4186151a9
Jeff Murray <jeff.murray@vertexvis.com>
2024-03-22 14:26:32 -0500

    Temporarily use utils for checking to publish

--- scripts/detect_release.sh
+++ scripts/detect_release.sh
@@ -7,7 +7,7 @@
 set -e
 
 version=$(get_version)
-published_version=`npm view @vertexvis/viewer --json versions | jq --arg version "$version" -r '.[] | select(. == $version)'`
+published_version=`npm view @vertexvis/utils --json versions | jq --arg version "$version" -r '.[] | select(. == $version)'`
 
 if test -z "$published_version"
 then
