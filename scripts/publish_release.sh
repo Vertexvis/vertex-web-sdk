c5488a29468b661627ea30fd889851635137b779
Jeff Murray <jeff.murray@vertexvis.com>
2024-03-22 14:31:31 -0500

    Tag again and use viewer for publish detection

--- scripts/publish_release.sh
+++ scripts/publish_release.sh
@@ -11,15 +11,15 @@ sha="$(git rev-parse HEAD)"
 
 npx lerna publish from-package --yes
 
-# curl -s -X POST https://api.github.com/repos/$REPOSITORY/releases \
-# -H "Authorization: token $GITHUB_TOKEN" \
-# -d @- <<EOF
-# {
-#   "tag_name": "$version",
-#   "target_commitish": "$sha",
-#   "name": "$version",
-#   "body": "Automated release for $version\n",
-#   "draft": false,
-#   "prelease": false
-# }
-# EOF
+curl -s -X POST https://api.github.com/repos/$REPOSITORY/releases \
+-H "Authorization: token $GITHUB_TOKEN" \
+-d @- <<EOF
+{
+  "tag_name": "$version",
+  "target_commitish": "$sha",
+  "name": "$version",
+  "body": "Automated release for $version\n",
+  "draft": false,
+  "prelease": false
+}
+EOF
