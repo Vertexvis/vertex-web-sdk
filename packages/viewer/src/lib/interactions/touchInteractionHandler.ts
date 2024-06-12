9b7c752b86e560e5a2b1f32baffab50d766e107d
Madison Ehlers <madison.ehlers@vertexvis.com>
2024-06-12 09:42:59 -0500

    remove unused code

--- packages/viewer/src/lib/interactions/touchInteractionHandler.ts
+++ packages/viewer/src/lib/interactions/touchInteractionHandler.ts
@@ -25,14 +25,6 @@ export class TouchInteractionHandler extends MultiTouchInteractionHandler {
     element.addEventListener('touchstart', this.handleTouchStart);
   }
 
-  public addEventListenersToElement(element: HTMLElement): void {
-    element.addEventListener('touchstart', this.handleTouchStart);
-  }
-
-  public disposeListenersOnElement(element?: HTMLElement): void {
-    element?.removeEventListener('touchstart', this.handleTouchStart);
-  }
-
   private handleTouchStart(event: TouchEvent): void {
     if (event.touches.length >= 1) {
       event.preventDefault();
