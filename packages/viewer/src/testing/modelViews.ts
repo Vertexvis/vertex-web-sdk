22992c380c2284cc977a7539ebc2d7a88d6c3674
Jeff Murray <jeff.murray@vertexvis.com>
2024-07-24 12:26:38 -0500

    Add tests

--- packages/viewer/src/testing/modelViews.ts
+++ packages/viewer/src/testing/modelViews.ts
@@ -1,6 +1,11 @@
 import { ModelView } from '@vertexvis/scene-view-protos/core/protos/model_views_pb';
-import { ListItemModelViewsResponse } from '@vertexvis/scene-view-protos/sceneview/protos/scene_view_api_pb';
+import { Uuid2l } from '@vertexvis/scene-view-protos/core/protos/uuid_pb';
+import {
+  ListItemModelViewsResponse,
+  UpdateSceneViewRequest,
+} from '@vertexvis/scene-view-protos/sceneview/protos/scene_view_api_pb';
 import { UUID } from '@vertexvis/utils';
+import { FieldMask } from 'google-protobuf/google/protobuf/field_mask_pb';
 
 import { random } from './random';
 import { makeUuid2l } from './sceneView';
@@ -11,6 +16,33 @@ export function makeListItemModelViewsResponse(
   const res = new ListItemModelViewsResponse();
   res.setModelViewsList(modelViews);
   return res;
+}
+
+export function makeUpdateSceneViewRequest(
+  sceneViewId: UUID.UUID,
+  modelViewId?: UUID.UUID
+): UpdateSceneViewRequest {
+  const req = new UpdateSceneViewRequest();
+
+  const svUuid = UUID.toMsbLsb(sceneViewId);
+  const svUuid2l = new Uuid2l();
+  svUuid2l.setMsb(svUuid.msb);
+  svUuid2l.setLsb(svUuid.lsb);
+  req.setModelViewId(svUuid2l);
+
+  if (modelViewId != null) {
+    const mvUuid = UUID.toMsbLsb(modelViewId);
+    const mvUuid2l = new Uuid2l();
+    mvUuid2l.setMsb(mvUuid.msb);
+    mvUuid2l.setLsb(mvUuid.lsb);
+    req.setModelViewId(mvUuid2l);
+  }
+
+  const mask = new FieldMask();
+  mask.addPaths('sceneView.modelViewId');
+  req.setUpdateMask(mask);
+
+  return req;
 }
 
 export function makeModelView(
