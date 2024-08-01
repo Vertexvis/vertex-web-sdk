bba6684d6bf6321c356d0d85124902cc3e15db56
Jeff Murray <jeff.murray@vertexvis.com>
2024-08-01 13:10:17 -0500

    Include camera in exported type

--- packages/viewer/src/lib/model-views/types.ts
+++ packages/viewer/src/lib/model-views/types.ts
@@ -1,11 +1,13 @@
 import { UUID } from '@vertexvis/utils';
 
+import { FrameCamera } from '../types';
 import { PagingLinks } from '../types/pagination';
 
 export interface ModelView {
   id: UUID.UUID;
   displayName: string;
   partRevisionId: UUID.UUID;
+  camera: FrameCamera.FrameCamera;
 }
 
 export interface ModelViewListResponse {
