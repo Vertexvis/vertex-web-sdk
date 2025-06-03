24a6205b999ddf23746bf2389fda2df61d5f63d6
amvertex <annastacia.mccarty@vertexvis.com>
2025-06-03 13:53:43 -0500

    Get scene view item

--- packages/viewer/src/lib/mappers/uuid.ts
+++ packages/viewer/src/lib/mappers/uuid.ts
@@ -1,8 +1,8 @@
 import { Uuid2l } from '@vertexvis/scene-view-protos/core/protos/uuid_pb';
-import { toMsbLsb, UUID } from '@vertexvis/utils/src/uuid';
+import { UUID } from '@vertexvis/utils';
 
-export function toUuid2l(id: UUID): Uuid2l {
-  const msbLsb = toMsbLsb(id);
+export function toUuid2l(id: UUID.UUID): Uuid2l {
+  const msbLsb = UUID.toMsbLsb(id);
   const pb = new Uuid2l();
   pb.setMsb(msbLsb.msb);
   pb.setLsb(msbLsb.lsb);
