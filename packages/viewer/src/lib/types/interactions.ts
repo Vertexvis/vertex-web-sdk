a6428490a52f8198bdf6a79d388eda89fe5b2c00
Annastacia McCarty <39739917+amvertex@users.noreply.github.com>
2025-10-21 15:14:44 -0500

    [PLAT-7331] Improve zooming with the mouse wheel (#702)
    * Improve zooming
    
    * Improve zooming
    
    * Improve zooming
    
    * Improve zooming
    
    * Improve zooming

--- packages/viewer/src/lib/types/interactions.ts
+++ packages/viewer/src/lib/types/interactions.ts
@@ -48,6 +48,6 @@ export const defaultInteractionConfig: InteractionConfig = {
   coarsePointerThreshold: 3,
   interactionDelay: 75,
   useMinimumPerspectiveZoomDistance: true,
-  mouseWheelInteractionEndDebounce: 350,
+  mouseWheelInteractionEndDebounce: 400,
   reverseMouseWheelDirection: false,
 };
