21db3091aeb190ae451e245021b9f289f0de7f3e
amvertex <annastacia.mccarty@vertexvis.com>
2026-04-27 15:33:13 -0500

    Update build-tools

--- internal/build/rollup.config.js
+++ internal/build/rollup.config.js
@@ -1,7 +1,7 @@
 import { config, input, output, typescript } from '@vertexvis/build-tools';
 
 export default config(
-    input('src/index.ts'),
-    typescript(),
-    output({ enableInlineDynamicImports: true })
+  input('src/index.ts'),
+  typescript(),
+  output({ enableInlineDynamicImports: true })
 );
