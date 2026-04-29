1a7586c2bce858cb70c0df5c91a497334864b5a8
Aaron Eischeid <a.eischeid@gmail.com>
2026-04-29 14:13:54 -0500

    remove applyPolyfills from the viewer-vue too

--- packages/viewer-vue/src/lib/plugin.ts
+++ packages/viewer-vue/src/lib/plugin.ts
@@ -1,10 +1,8 @@
-import { applyPolyfills, defineCustomElements } from '@vertexvis/viewer/loader';
+import { defineCustomElements } from '@vertexvis/viewer/loader';
 import { Plugin } from 'vue';
 
 export const VertexViewerPlugin: Plugin = {
   async install() {
-    applyPolyfills().then(() => {
-      defineCustomElements();
-    });
+    defineCustomElements();
   },
 };
