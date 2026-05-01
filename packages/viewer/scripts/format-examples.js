6cb44997a3415705b8740f6c4718b317d9b806a1
Aaron Eischeid <a.eischeid@gmail.com>
2026-05-01 11:08:28 -0500

    use explict prettier deb more directly

--- packages/viewer/scripts/format-examples.js
+++ packages/viewer/scripts/format-examples.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-import { execFileSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
-import { createRequire } from 'module';
+import prettier from 'prettier';
 import { fileURLToPath } from 'url';
 
 // This helper runs as part of viewer postbuild because Stencil generates
@@ -10,35 +9,30 @@ import { fileURLToPath } from 'url';
 // reformat them against the repo Prettier config before publishing.
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const require = createRequire(import.meta.url);
 const rootDir = path.resolve(__dirname, '../../..');
 const prettierConfigPath = path.join(rootDir, '.prettierrc.json');
-const prettierCliPath = require.resolve('prettier/bin/prettier.cjs');
+const prettierConfig = await prettier.resolveConfig(prettierConfigPath);
 
 const stdin = fs.readFileSync(process.stdin.fd, 'utf-8');
 
 const docExampleMatches = stdin.matchAll(/```([^ ]*) (.*) ```/g);
 const docMatches = stdin.matchAll(/[ ]*[*](.*)/g);
-const typeDefFormattedExamples = [...docExampleMatches].reduce(function (
-  typeDefFormattedExamples,
-  exampleMatch
-) {
+let typeDefFormattedExamples = stdin;
+for (const exampleMatch of docExampleMatches) {
   const type = exampleMatch[1];
 
-  const formattedMatch = `\`\`\`${type}\n${formatExample(
+  const formattedMatch = `\`\`\`${type}\n${await formatExample(
     exampleMatch[2],
     type !== '' ? type : undefined
   )}\`\`\``.replace(/^/gm, '* ');
 
-  return typeDefFormattedExamples.replace(
+  typeDefFormattedExamples = typeDefFormattedExamples.replace(
     exampleMatch[0],
     `\n${formattedMatch}`
   );
-}, stdin);
-const typeDefFormattedMultiline = [...docMatches].reduce(function (
-  typeDefFormattedMultiline,
-  mutilineMatch
-) {
+}
+let typeDefFormattedMultiline = typeDefFormattedExamples;
+for (const mutilineMatch of docMatches) {
   const indentationMatch = mutilineMatch[0].match(/([ ]*)[*]/);
   const indentation = indentationMatch[1] == null ? '' : indentationMatch[1];
   const formattedMatch = `${indentation}*${mutilineMatch[1].replace(
@@ -46,29 +40,17 @@ const typeDefFormattedMultiline = [...docMatches].reduce(function (
     `$1\n${indentation}*\n${indentation}* $2`
   )}`;
 
-  return typeDefFormattedMultiline.replace(
+  typeDefFormattedMultiline = typeDefFormattedMultiline.replace(
     mutilineMatch[0],
     `${formattedMatch}`
   );
-}, typeDefFormattedExamples);
+}
 
 process.stdout.write(typeDefFormattedMultiline);
 
-function formatExample(example, parser = 'typescript') {
-  return String(
-    execFileSync(
-      process.execPath,
-      [
-        prettierCliPath,
-        '--parser',
-        parser,
-        '--config',
-        prettierConfigPath,
-      ],
-      {
-        input: example,
-        cwd: rootDir,
-      }
-    )
-  );
+async function formatExample(example, parser = 'typescript') {
+  return await prettier.format(example, {
+    ...prettierConfig,
+    parser,
+  });
 }
