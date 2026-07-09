8fafaf2b04f77a1e3ee471e8d2f5ab4469a5b39e
Aaron Eischeid <aaron.eischeid@vertex3d.com>
2026-07-09 13:31:32 -0500

    micro optimize buildFlyToOperation
    as it is called frequently in certain render loops like scrollwheel
    zooming

--- packages/viewer/src/lib/scenes/mapper.ts
+++ packages/viewer/src/lib/scenes/mapper.ts
@@ -298,65 +298,47 @@ export function buildFlyToOperation(
   animation?: Animation.Animation,
   baseCamera?: FrameCamera.FrameCamera,
 ): vertexvis.protobuf.stream.IFlyToPayload {
-  const payload = {
-    frameCorrelationId: {
-      value: frameCorrelationId,
-    },
-    animation: animation
-      ? {
-          duration: toProtoDuration(animation.milliseconds),
-        }
-      : undefined,
-    baseCamera:
-      baseCamera != null && isValidFrameCamera(baseCamera)
-        ? FrameCamera.toProtobuf(baseCamera)
-        : undefined,
+  const flyTo = options.flyTo;
+  const payload: vertexvis.protobuf.stream.IFlyToPayload = {
+    frameCorrelationId: { value: frameCorrelationId },
   };
 
-  switch (options.flyTo.type) {
-    case 'supplied': {
-      return {
-        ...payload,
-        itemSuppliedId: options.flyTo.data,
-      };
-    }
-    case 'internal': {
-      return {
-        ...payload,
-        itemId: new vertexvis.protobuf.core.Uuid({
-          hex: options.flyTo.data,
-        }),
-      };
-    }
-    case 'camera': {
-      return {
-        ...payload,
-        camera: FrameCamera.toProtobuf(options.flyTo.data),
-      };
-    }
-
-    case 'scene-item-query':
-      return {
-        ...payload,
-        sceneItemQueryExpression: options.flyTo.data,
-      };
-
-    case 'bounding-box': {
-      return {
-        ...payload,
-        boundingBox: {
-          xmin: options.flyTo.data.min.x,
-          xmax: options.flyTo.data.max.x,
-          ymin: options.flyTo.data.min.y,
-          ymax: options.flyTo.data.max.y,
-          zmin: options.flyTo.data.min.z,
-          zmax: options.flyTo.data.max.z,
-        },
-      };
-    }
-    default:
-      return {};
+  if (animation != null) {
+    payload.animation = {
+      duration: toProtoDuration(animation.milliseconds),
+    };
   }
+
+  if (baseCamera != null && isValidFrameCamera(baseCamera)) {
+    payload.baseCamera = FrameCamera.toProtobuf(baseCamera);
+  }
+
+  switch (flyTo.type) {
+    case 'supplied':
+      payload.itemSuppliedId = flyTo.data;
+      break;
+    case 'internal':
+      payload.itemId = new vertexvis.protobuf.core.Uuid({ hex: flyTo.data });
+      break;
+    case 'camera':
+      payload.camera = FrameCamera.toProtobuf(flyTo.data);
+      break;
+    case 'scene-item-query':
+      payload.sceneItemQueryExpression = flyTo.data;
+      break;
+    case 'bounding-box':
+      payload.boundingBox = {
+        xmin: flyTo.data.min.x,
+        xmax: flyTo.data.max.x,
+        ymin: flyTo.data.min.y,
+        ymax: flyTo.data.max.y,
+        zmin: flyTo.data.min.z,
+        zmax: flyTo.data.max.z,
+      };
+      break;
+  }
+
+  return payload;
 }
 
 function buildOperationTypes(
