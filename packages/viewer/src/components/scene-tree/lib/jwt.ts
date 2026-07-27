c047bb098c356d82b0bbaff1ddae2e0dd8d7eda4
Aaron Eischeid <aaron.eischeid@vertex3d.com>
2026-07-27 11:20:36 -0500

    upgrade jwt-decode from v3 to v4

--- packages/viewer/src/components/scene-tree/lib/jwt.ts
+++ packages/viewer/src/components/scene-tree/lib/jwt.ts
@@ -1,4 +1,4 @@
-import jwtDecode, { JwtPayload } from 'jwt-decode';
+import { jwtDecode, type JwtPayload } from 'jwt-decode';
 
 export interface SceneTreeJwtToken extends JwtPayload {
   view: string;
